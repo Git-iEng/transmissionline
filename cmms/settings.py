81fee0b8a4695ed8402aadded8b8fed73866f1e1
sujay-iEng <sujay@iengaust.com.au>
2026-03-30 14:58:39 +0530

    captcha 3

--- cmms/settings.py
+++ cmms/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tpzqxw&&@03wq2yzgf!gzh6u2=044s2j+_!#jioe(#f^6%quzo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     SECURE_SSL_REDIRECT = False
@@ -35,7 +35,7 @@ else:
     SESSION_COOKIE_SECURE = True
 
 
-ALLOWED_HOSTS = ["transmissionline.ieng.tech", ".ieng.tech", "*"]
+ALLOWED_HOSTS = ["transmissionline.ieng.tech", ".ieng.tech"]
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 CSRF_TRUSTED_ORIGINS = ["https://*.ieng.tech"]
